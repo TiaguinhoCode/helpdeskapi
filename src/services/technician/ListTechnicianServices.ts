c0767ddf41e73575adfef22acd1055e510afc775
PlayTi <tiago.rafael019@gmail.com>
2024-05-31 16:15:12 -0300

    Ajeitando filtro de tecnico

--- src/services/technician/ListTechnicianServices.ts
+++ src/services/technician/ListTechnicianServices.ts
@@ -5,7 +5,10 @@ class ListTechnicianServices {
     async execute() {
         const user = await prismaClient.user.findMany({
             where: {
-                department_id: "5953de49-17a4-40c1-a0df-8d2474a0e735"
+                department_id: "5953de49-17a4-40c1-a0df-8d2474a0e735",
+                NOT: {
+                    id: "3fc98d16-0a52-48c3-bd61-fe4f44a99131"
+                }
             },
             select: {
                 id: true,
